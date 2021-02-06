1d9bd088e28379b0504bc14428cd894f0bba9345
Vikas Ukani <vikasukani5@gmail.com>
2021-02-06 16:36:40 +0530

    Deploy to page

--- out/_next/static/development/_buildManifest.js
+++ out/_next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/use":["static\u002Fchunks\u002Fpages\u002Fuse.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fuse"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
