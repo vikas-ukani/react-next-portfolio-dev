5b2794f3b8bdca0d28bb185ef1b6940c0a83daab
Vikas Ukani <vikasukani5@gmail.com>
2021-03-19 01:02:50 +0530

    Application Lauout uppdate and Loader change

--- Components/UI/Loader.jsx
+++ Components/UI/Loader.jsx
@@ -1,5 +1,7 @@
-export default Loader = () => (
+const Loader = () => (
     <div className="doc-loader" >
         <img src={process.env.LINK + "/images/balls-loader.gif"} alt="Loading..." />
     </div>
-)
+);
+
+export default Loader;
