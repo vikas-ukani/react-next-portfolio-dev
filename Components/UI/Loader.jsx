8a67dfcd95d9211ac95b1b14eb0d5ec54971ba53
Vikas Ukani <vikasukani5@gmail.com>
2021-04-02 10:41:31 +0530

    Page sertup.

--- Components/UI/Loader.jsx
+++
@@ -1,7 +0,0 @@
-const Loader = () => (
-    <div className="doc-loader" >
-        <img src={process.env.LINK + "/images/balls-loader.gif"} alt="Loading..." />
-    </div>
-);
-
-export default Loader;
