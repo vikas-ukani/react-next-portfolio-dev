35101a16dc442426ad000805c5b36c97bc856df9
Vikas Ukani <vikasukani5@gmail.com>
2021-04-02 13:15:49 +0530

    Deployment In-Progress...

---
+++ _next/static/webpack/pages/index.fa45dcfa7243d2d7aa53.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./data.json":
+/*!*******************!*\
+  !*** ./data.json ***!
+  \*******************/
+/*! exports provided: projects, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"projects\\\":[{\\\"id\\\":1,\\\"name\\\":\\\"php 1\\\",\\\"category\\\":\\\"php\\\"},{\\\"id\\\":1,\\\"name\\\":\\\"javascript 2\\\",\\\"category\\\":\\\"javascript\\\"},{\\\"id\\\":1,\\\"name\\\":\\\"python 3\\\",\\\"category\\\":\\\"python\\\"},{\\\"id\\\":1,\\\"name\\\":\\\"php 4\\\",\\\"category\\\":\\\"php\\\"},{\\\"id\\\":1,\\\"name\\\":\\\"javascript 5\\\",\\\"category\\\":\\\"javascript\\\"},{\\\"id\\\":1,\\\"name\\\":\\\"python 6\\\",\\\"category\\\":\\\"python\\\"},{\\\"id\\\":1,\\\"name\\\":\\\"machine 64\\\",\\\"category\\\":\\\"machine\\\"},{\\\"id\\\":11,\\\"name\\\":\\\"machine 11\\\",\\\"category\\\":\\\"machine\\\"},{\\\"id\\\":1,\\\"name\\\":\\\"javascript 53\\\",\\\"category\\\":\\\"javascript\\\"},{\\\"id\\\":1,\\\"name\\\":\\\"python 54asd\\\",\\\"category\\\":\\\"python\\\"},{\\\"id\\\":1,\\\"name\\\":\\\"javascript asd\\\",\\\"category\\\":\\\"javascript\\\"}]}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL2RhdGEuanNvbi5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./data.json\n");
+
+/***/ })
+
+})
