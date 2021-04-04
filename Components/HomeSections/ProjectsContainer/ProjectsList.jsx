5576760e59cca5cc2e8da4686f78b80ff67edbe6
Vikas Ukani <vikasukani5@gmail.com>
2021-04-04 12:35:07 +0530

    Projects details infor and design changes.

--- Components/HomeSections/ProjectsContainer/ProjectsList.jsx
+++ Components/HomeSections/ProjectsContainer/ProjectsList.jsx
@@ -1,5 +1,5 @@
 import Slider from "react-slick";
-
+import Link from 'next/link'
 // const SampleNextArrow = (props) => {
 //     const { className, style } = props;
 //     return (
@@ -14,7 +14,7 @@ const ProjectsList = ({ projects }) => {
         centerMode: true,
         infinite: true,
         swipeToSlide: true,
-        autoplay: true,
+        autoplay: false,
 
         centerPadding: "60px",
         slidesToShow: 2,
@@ -28,22 +28,49 @@ const ProjectsList = ({ projects }) => {
             <Slider {...settings}>
                 {projects.map((project, key) => (
                     <div key={key} id={key}
-                        className="grid-item element-item p_one_third transition duration-500 ease-in-out transform  hover:-translate-y-1 hover:scale-110"
+                        className="grid-item element-item p_one_third transition duration-500 ease-in-out transform  hover:-translate-y-1 hover:scale-105 hover:rounded-lg "
                     >
-                        <a className="item-link"
-                            href=""
-                            data-id={key}
-                        >
-                            <img width="500" height="400" src='https://picsum.photos/500/500'
-                                className="attachment-post-thumbnail size-post-thumbnail wp-post-image"
-                                alt={project.name}
-                                sizes="(max-width: 500px) 80vw, 500px" />
-                            <p className="portfolio-text text-center ">
-                                <small
-                                    onMouseEnter={e => e.target.className = 'text-light-green'}
-                                    onMouseLeave={e => e.target.className = 'text-light-orange'}>{project.name}</small>
-                            </p>
-                        </a>
+                        <div className="max-w-sm overflow-hidden shadow-lg bg-light-dark text-white w-full mb-6 rounded-lg">
+                            {project.image_url && (
+                                <img className="w-full " src={"https://raw.githubusercontent.com/vikas-ukani/photo-point/master/public/images/photo-point-image.jpg?token=ANXLAGUOKZYBVMGMD5QGJLTANBT3I"} height="400" width="500" alt={project.name} />
+                            )}
+                            {!project.image_url && (
+                                <img className="w-full min-h-full" src="https://picsum.photos/400/400" width="400" height="400" alt="Mountain" />
+                            )}
+                            <div className="px-6 py-4">
+                                {/* If url found then */}
+                                {project.url && (
+                                    <Link href="">
+                                        <a >
+                                            <div className="font-bold text-xl mb-2 text-center">{project.name}</div>
+                                        </a>
+                                    </Link>
+
+                                )}
+                                {/* If url not found then */}
+                                {!project.url && (
+                                    <div className="font-bold text-xl mb-2 text-center">{project.name}</div>
+                                )}
+                                {project.short_description &&
+                                    (
+                                        <p className="text-gray-700 text-base">
+                                            {project.short_description}
+                                        </p>
+                                    )
+                                }
+                            </div>
+                            {project.tags && (
+                                <div className="px-6 pt-4 pb-2">
+                                    {project.tags.map((tag, key) => (
+                                        <span key={key} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                                            #{tag}
+                                        </span>
+                                    )
+                                    )}
+                                </div>
+                            )}
+
+                        </div>
                     </div>
                 ))}
             </Slider>
