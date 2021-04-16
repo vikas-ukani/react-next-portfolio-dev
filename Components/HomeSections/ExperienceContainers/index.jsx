2d34bdccfc3acea5d656365792afbd7feadd4c22
Vikas Ukani <vikasukani5@gmail.com>
2021-04-16 23:57:03 +0530

    Experience Holder Change

--- Components/HomeSections/ExperienceContainers/index.jsx
+++ Components/HomeSections/ExperienceContainers/index.jsx
@@ -1,5 +1,5 @@
 import MavenExperience from './MavenExperience'
-import DignizantExperience from './DignixantExperience'
+import DignizantExperience from './DignizantExperience'
 import CoruscateExperience from './CoruscateExperience'
 
 export default function ExperienceContainers() {
